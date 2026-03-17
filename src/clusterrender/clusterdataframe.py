f1811323cb747a30f9f0f65925d23ec232371e19
tinatn29 <tanaporn.nanarong@gmail.com>
2026-03-17 13:47:09 -0400

    add from_gt_row and add_lower_shell methods

--- src/clusterrender/clusterdataframe.py
+++ src/clusterrender/clusterdataframe.py
@@ -5,6 +5,7 @@ format.
 """
 
 import pandas as pd
+import numpy as np
 
 # -------------------------
 
@@ -33,7 +34,7 @@ class ClusterDataFrame(pd.DataFrame):
     def _constructor(self):
         return ClusterDataFrame
 
-    def __init__(self, data=None, *args, **kwargs):
+    def __init__(self, data=None, central_atom=None, *args, **kwargs):
         """Initialize ClusterDataFrame with DataFrame-compatible arguments."""
         if kwargs.get("copy") is None and isinstance(data, pd.DataFrame):
             kwargs.update(copy=True)
@@ -42,3 +43,61 @@ class ClusterDataFrame(pd.DataFrame):
 
         if isinstance(data, pd.DataFrame) and data.attrs:
             self.attrs = data.attrs
+
+        if central_atom is not None:
+            self.central_atom = central_atom
+
+    @classmethod
+    def from_groundtruth_row(cls, row, central_atom):
+        """Create a ClusterDataFrame from a row of a ground truth DataFrame."""
+        from utils.row_parser import parse_groundtruth_row
+
+        df = parse_groundtruth_row(row, central_atom)
+        return cls(df, central_atom=central_atom)
+
+    def add_closest_lower_shell_neighbor(self):
+        """Add a column for the closest lower shell neighbor."""
+        if "shell" not in self.columns:
+            raise ValueError(
+                "ClusterDataFrame must have 'shell' column to add \
+                    closest lower shell neighbor."
+            )
+
+        if "site_name" not in self.columns:
+            # add site_name column if it doesn't exist
+            # sort first by distance to ensure correct site_name assignment
+            self["distance"] = (
+                self["x"] ** 2 + self["y"] ** 2 + self["z"] ** 2
+            ) ** 0.5
+            self.sort_values(by="distance").reset_index(
+                drop=True, inplace=True
+            )
+            self["site_name"] = self.apply(
+                lambda row: f"{row['species']}_{row['shell']}_{row.name}",
+                axis=1,
+            )
+
+        def find_closest_lower_shell_neighbor(row):
+            if row["shell"] == 0:
+                return "center"
+            else:
+                lower_shell_neighbors = self[self["shell"] == row["shell"] - 1]
+
+                if lower_shell_neighbors.empty:
+                    return None
+                elif len(lower_shell_neighbors) == 1:
+                    return lower_shell_neighbors.iloc[0]["site_name"]
+                else:
+                    distances = np.sqrt(
+                        (lower_shell_neighbors["x"] - row["x"]) ** 2
+                        + (lower_shell_neighbors["y"] - row["y"]) ** 2
+                        + (lower_shell_neighbors["z"] - row["z"]) ** 2
+                    )
+                    closest_neighbor_index = distances.idxmin()
+                    return lower_shell_neighbors.loc[closest_neighbor_index][
+                        "site_name"
+                    ]
+
+        self["nearest_lower_shell_neighbor"] = self.apply(
+            find_closest_lower_shell_neighbor, axis=1
+        )
